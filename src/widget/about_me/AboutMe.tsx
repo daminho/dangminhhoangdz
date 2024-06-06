d1a998594bf2ed6f5e5ecd360da07d7e57e9c12b
Hoang <dangminhhoangdz@gmail.com>
2024-06-06 20:45:33 +0900

    Update about me, Luveri's logo

--- src/widget/about_me/AboutMe.tsx
+++ src/widget/about_me/AboutMe.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@mui/material";
 import resume from './CV_HoangDang.pdf'
-
+import react from "react";
 
 
 
@@ -12,10 +12,12 @@ export default function AboutMe() {
         <div style = {{display: "block", paddingLeft: "32px", paddingRight: "32px", marginTop: "12px"}}>
            <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
             <div style= {{fontSize: "18px", fontFamily:  'monospace', color: "#545454"}}> I'm Dang Minh Hoang, a Vietnamese student studying double major in Computer Science and Business Technology Management at KAIST, South Korea.
-                 My life goal is to build valuable softwares/products for the societies. Currently, I'm interested in Human-Computer Interaction</div>
+                 My life goal is to build useful products to contribute to the society. Currently, I'm interested in <span style={{fontWeight: "bold"}}>Efficiency for ML models</span> and <span style={{fontWeight: "bold"}}>Human-computer Interaction</span>
+                 . I hope to bring more useful application of AI into the daily life.
+            </div>
             {divider}
-            <div style= {{fontSize: "18px", fontFamily:  'monospace', color: "#545454"}}> I'm a dynamic, energetic, and optimistic person. I love playing billiards, piano, and dancing. Apart from energetic moments, I spend my self-time on reading books,
-                cafe hopping, and going to chilling-vibe locations  </div>
+            <div style= {{fontSize: "18px", fontFamily:  'monospace', color: "#545454"}}> I'm a dynamic, energetic, durable, and optimistic person. I love playing billiards, piano, and dancing. I can play billiards for 10 hours straight and usually play 8-ball.
+                 Apart from the energetic moments, I spend my self-time on reading books, cafe hopping, and riding my bike to a random place</div>
            </div>
            <Button href ={resume} target="blank" sx = {{padding: "4px", margin: "0px", marginTop: "12px"}} variant="contained">
                 My CV
